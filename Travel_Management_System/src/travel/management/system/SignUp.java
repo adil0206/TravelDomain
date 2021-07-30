8ae9e2dff7341e79e63926894c353fcac378bedc
Prashant <Prashant@DESKTOP-TD5TPD0>
2021-07-30 21:43:36 +0530

    Signup Page is created by Prashant

--- Travel_Management_System/src/travel/management/system/SignUp.java
+++ Travel_Management_System/src/travel/management/system/SignUp.java
@@ -3,8 +3,7 @@ package travel.management.system;
 public class SignUp {
 
 	public static void main(String[] args) {
-		System.out.println("This is SignUp Page");
-
+		System.out.println("This is SignUp Page by prashant");
 	}
 
 }
