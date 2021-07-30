668266fd9d5791534ae6dc481a2d8649932831ed
adil0206 <nit.adilkhan0206@gmail.com>
2021-07-30 21:20:52 +0530

    Modified Splash.java by TL

--- Travel_Management_System/src/travel/management/system/Splash.java
+++ Travel_Management_System/src/travel/management/system/Splash.java
@@ -5,7 +5,7 @@ public class Splash {
 	public static void main(String[] args) {
 		System.out.println("Hello Wordl !!");
 		System.out.println("Hello from prashant !!");
-System.out.println("Prashant");
+		System.out.println("Prashant");
 	}
 
 }
