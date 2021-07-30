a24a8c22f691d3502c22c9115b03a77d6c02611c
adil0206 <nit.adilkhan0206@gmail.com>
2021-07-30 12:03:46 +0530

    Modified Splash.java

--- Travel_Management_System/src/travel/management/system/Splash.java
+++ Travel_Management_System/src/travel/management/system/Splash.java
@@ -3,7 +3,7 @@ package travel.management.system;
 public class Splash {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Wordl !!");
 
 	}
 
