1f90495d79a584f5e89259602f4ffcd32f06295d
adil0206 <nit.adilkhan0206@gmail.com>
2021-07-31 10:03:33 +0530

    Set Up Project 1 - Travel Management System By TL

--- Travel_Management_System/src/travel/management/system/SignUp.java
+++
@@ -1,9 +0,0 @@
-package travel.management.system;
-
-public class SignUp {
-
-	public static void main(String[] args) {
-		System.out.println("This is SignUp Page by prashant");
-	}
-
-}
--- Travel_Management_System/src/travel/management/system/Splash.java
+++ Travel_Management_System/src/travel/management/system/Splash.java
@@ -3,10 +3,7 @@ package travel.management.system;
 public class Splash {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("Adil");
-		System.out.println("Prashant is the best developer in the world");
-		System.out.println("Hello Friends by PL");
+	
 	}
 
 }
