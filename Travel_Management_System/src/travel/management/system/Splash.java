b356c4f62bb96a0b142cefd6cf75fc5c83bd7d98
adil0206 <nit.adilkhan0206@gmail.com>
2021-07-31 10:08:13 +0530

    Clean Splash.java by TL

--- Travel_Management_System/src/travel/management/system/Splash.java
+++ Travel_Management_System/src/travel/management/system/Splash.java
@@ -3,12 +3,6 @@ package travel.management.system;
 public class Splash {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-
-
-=======
-	
->>>>>>> refs/heads/Proj1-TravelManagementSystem
 	}
 
 }
